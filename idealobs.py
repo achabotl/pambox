a8fec5ed77c1cf108888d2952f0742f7b923947f
Alexandre Chabot-Leclerc <alexandre.chabot.leclerc@gmail.com>
2013-09-05 13:24:10 +0200

    Port of matlab ideal observer
    With tests to validate similar implementation than in Matlab.

---
+++ idealobs.py
@@ -0,0 +1,73 @@
+from scipy.stats import norm
+from scipy.optimize import leastsq
+from scipy.special import erfc
+from numpy import sqrt
+from numpy import arange
+from numpy import array
+from numpy import linspace
+
+
+# Defaults
+K = sqrt(1.2)
+Q = 0.5
+SIGMA_S = 0.6
+M = 8000.0
+
+
+def snrenv_to_pc(snrenv, k=K, q=Q, sigma_s=SIGMA_S, m=M):
+    """Convert SNR_env values to a percent correct using a ideal observer
+    snrenv: linear values of SNRenv
+    Reference: Jorgensen and Dau (2011)
+    Author: Alexandre Chabot-Leclerc (alech AT elektro.dtu.dk)
+    """
+    Un = norm.ppf(1.0 - 1.0 / m)
+    sn = 1.28255 / Un
+    un = Un + 0.577 / Un
+    dp = k * snrenv**q
+    return norm.cdf(dp, un, sqrt(sigma_s**2 + sn**2)) * 100
+
+
+def fit_obs(snrenv, pcdata):
+    """Find the optimal parameters for the ideal observer.
+    snrenv: the linear SNRenv values that are to be converted to percent
+        correct.
+    pcdata: the data, in percentage of correctly understood tokens.
+    """
+    errfc = lambda p, snr, data: snrenv_to_pc(snrenv, p[0], p[1], p[2], M) - data
+    p0 = [K, Q, SIGMA_S]
+    res = leastsq(errfc, p0, args=(snrenv, pcdata))
+    return res
+
+
+def psy_fn(x, mu=0, sigma=1):
+    """Psychometric function
+    Convert x values to percent correct
+    Inputs:
+    x: the input array
+    mu: mean of the distribution (default 0)
+    sigma: standard deviation of the distribution (default 1)
+
+    Outputs:
+    pc: array of values between 0 and 100
+    """
+    return erfc(-(x - mu) / (sqrt(2) * sigma)) / 2
+
+
+if __name__ == '__main__':
+    from pylab import plot, figure, show, xlabel, ylabel, legend
+    snr = arange(-9, 3, 1)
+    snrenv = 10 ** linspace(-2, 2, len(snr))
+    data = psy_fn(snr, -3.1, 2.13) * 100
+    popt = fit_obs(snrenv, data)
+
+    figure
+    plot(snr, data, 'b--', label='Data')
+    plot(snr, snrenv_to_pc(snrenv, *popt[0], m=M), 'r--', label='Model')
+    xlabel('SNR [dB]')
+    ylabel('Percent correct')
+    legend(loc='upper left')
+    show()
+    k, q, ss = popt[0]
+    print(('Optimized parameters found:\nk = {:.3f}\nq = {:.3f}\n'
+           'sigma_s = {:.3f}\nm = {:.3f}').format(k, q, ss, M))
+
---
+++ test_idealobs.py
@@ -0,0 +1,50 @@
+import pytest
+import scipy.io as sio
+import idealobs
+import numpy as np
+
+
+@pytest.fixture
+def data():
+    return np.array([0.28032187,   1.07108181,   3.35513227,   8.66774961,
+                     18.61914334,  33.63172026,  51.87228063,  69.72236134,
+                     83.79127082,  92.72205919,  97.28779782,  99.16754416])
+
+
+@pytest.fixture
+def idealobs_parameters():
+    return (3.74647303e+00, 5.15928999e-02, -9.09197905e-07, 8000.)
+
+
+@pytest.fixture
+def snr():
+    return np.arange(-9, 3, 1)
+
+
+@pytest.fixture
+def snrenv(snr):
+    return 10. ** np.linspace(-2, 2, len(snr))
+
+
+def test_fit_obs(data, snrenv, idealobs_parameters):
+    popt = idealobs.fit_obs(snrenv, data)
+    np.testing.assert_allclose(popt[0], idealobs_parameters[0:3], atol=1e-5)
+
+
+def test_psy_fn():
+    mat = sio.loadmat('./test_files/test_psychometric_function.mat')
+    x = mat['x'][0]
+    mu = 0.
+    sigma = 1.0
+    target = mat['p'][0]
+    y = idealobs.psy_fn(x, mu, sigma)
+    np.testing.assert_allclose(y, target)
+
+
+def test_snr_env_to_pc(snrenv, idealobs_parameters, data):
+    pc = idealobs.snrenv_to_pc(np.arange(0, 21), 1, 0.5, 0.6, 8000.)
+    target = np.array([0.0000, 0.0025, 0.0267, 0.1327, 0.4403, 1.1314, 2.4278,
+                       4.5518, 7.6788, 11.8990, 17.1955, 23.4442, 30.4320,
+                       37.8885, 45.5214, 53.0503, 60.2323, 66.8786, 72.8613,
+                       78.1116, 82.6125])
+    np.testing.assert_allclose(pc, target, atol=1e-4)
