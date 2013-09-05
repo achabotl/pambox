0cf283ff2f3fe1252a6175d6c799e450f2648702
Alexandre Chabot-Leclerc <alexandre.chabot.leclerc@gmail.com>
2013-09-05 11:31:37 +0200

    Pass test on lowpass envelope filtering.

---
+++ test_auditory.py
@@ -0,0 +1,14 @@
+import pytest
+import numpy as np
+import auditory as aud
+import scipy.io as sio
+
+
+def test_lowpass_filtering_of_envelope():
+    mat = sio.loadmat("./test_files/test_envelope_filtering.mat")
+    envelope = mat['envelope']
+    target = mat['filtered_envelope']
+    filtered_envelope = aud.lowpass_env_filtering(envelope, 150., 1, 22050.)
+    np.testing.assert_allclose(filtered_envelope, target, atol=1e-7)
+
+
