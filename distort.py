b4a144cbf2ad41cd4ed5cabb637bf873ff45798b
Alexandre Chabot-Leclerc <alexandre.chabot.leclerc@gmail.com>
2013-09-12 17:55:06 +0200

    Mix noise and signal at given SNR

---
+++ distort.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy as sp
+import general
+
+
+def mix_noise(clean, noise, snr=None):
+    """Mix a signal signal noise at a given signal-to-noise ratio
+
+    :x: ndarray, clean signal.
+    :noise: ndarray, noise signal.
+    :snr: float, signal-to-noise ratio. If ignored, no noise is mixed, i.e.
+          clean == mix.
+    :returns: tuple, returns the clean signal, the mixture, and the noise
+              alone.
+
+    """
+    N = len(clean)
+    # Pick a random section of the noise
+    nNoise = len(noise)
+    # Select a random portion of the noise.
+    startIdx = np.random.randint(nNoise - N)
+    noise = noise[startIdx:startIdx + N]
+
+    # Get speech level and set noise level accordingly
+    clean_level = general.dbspl(clean)
+    noise = general.setdbspl(noise, clean_level - snr)
+
+    if snr:
+        mix = clean + noise
+    else:
+        mix = clean
+
+    return clean, mix, noise
+
