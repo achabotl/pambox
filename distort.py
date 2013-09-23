eacdf0c2dacd1ba17b976e35eb232653e0e2eb74
Alexandre Chabot-Leclerc <alexandre.chabot.leclerc@gmail.com>
2013-09-23 16:00:48 +0200

    Add spectral subtraction function with test.
    It's a correct implementation of the spectral subtraction function, like
    in the sEPSM, but think it could be "improved". For example, it does not
    use the period window for the hanning window.
    
    Also, I do not use the same indexes to set the noise estimate to zero;
    it does make sense to me. Why would I set to zero the same indexes as in
    the speech signal, if the whole point is to keep values above zero. It
    creates errors when doing the square root. However, it does make sense
    to apply exactly the same processing to the noise and to the speech.

--- distort.py
+++ distort.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
 import general
+import scipy.fftpack
 
 
 def mix_noise(clean, noise, snr=None):
@@ -43,6 +44,90 @@ def phase_jitter(x, a):
     """
     N = len(x)
     return x * np.cos(2 * np.pi * a * np.random.random_sample(N))
+
+
+def spec_sub(x, noise, factor, w=1024/2., padz=1024/2., shift_p=0.5):
+    """Apply spectral subtraction to a signal
+
+    Typical values of the parameters, for a sampling frequency of 44100 Hz
+    W = 1024
+    padz = 1024; %zero padding (pad with padz/2 from the left and padz/2 from
+    the right )Note that (W+padz) is the final frame window and hence the fft
+    length (it is normally chose as a power of 2)
+    shift_p = 0.5 # %Shift percentage is 50%
+
+    :x: ndarray, signal
+    :noise: ndarray, noise
+    :factor: float, the over-subtraction factor
+    :w: int, frame length
+    :padz: int, zero padding (pad with padz/2 from the left and the right)
+    :shift_p: float, shift percentage (overlap)
+    :returns: tuple of ndarrays, estimate of clean signal and estimate of
+              noisy signal.
+
+    """
+    wnd = np.hanning(w + 2)  # create hanning window with length = W
+    wnd = wnd[1:-1]
+
+    stim = np.vstack((x, noise))
+
+    len_signal = stim.shape[-1]  # Signal length
+    shift_p_indexes = np.floor(w * shift_p)
+    n_segments = np.floor((len_signal - w) / shift_p_indexes + 1)
+    len_segment = w + padz * 2 * shift_p
+    y = np.empty((2, n_segments, len_segment))
+    # Initialize arrays for spectral subtraction. Use only positive
+    # frequencies.
+    Y_hat = np.empty((n_segments, len_segment / 2 + 1))
+    PN_hat = Y_hat.copy()
+
+    # For each signal
+    for k in range(2):
+        # CUT THE APPROPRIATE SIGNAL FRAMES
+        indexes = np.tile(np.arange(w), (n_segments, 1))
+        index_shift = np.arange(n_segments) * shift_p_indexes
+        indexes = indexes + index_shift[:, np.newaxis]
+        y_tmp = stim[k]
+        y_tmp = y_tmp[indexes.astype('int')] * wnd
+        # PAD WITH ZEROS
+        pad = np.zeros((n_segments, padz / 2))
+        y_pad = np.hstack((pad, y_tmp, pad))
+        y[k, :, :] = y_pad
+
+    # FREQUENCY DOMAIN
+
+    # signal:
+    Y = sp.fftpack.fft(y[0])
+    #YY = Y(1:round(end/2)+1,:); # Half window (exploit the symmetry)
+    YY = Y[:, :(len_segment / 2 + 1)]  # Half window (exploit the symmetry)
+    YPhase = np.angle(YY)  # Phase
+    Y1 = np.abs(YY)  # Spectrum
+    Y2 = Y1 ** 2  # Power Spectrum
+
+    #  noise:
+    Y_N = sp.fftpack.fft(y[1])
+    YY_N = Y_N[:, :(len_segment / 2 + 1)]  # Half window (exploit the symmetry)
+    Y_NPhase = np.angle(YY_N)  # Phase
+    Y_N1 = np.abs(YY_N)  # Spectrum
+    Y_N2 = Y_N1 ** 2  # Power Spectrum
+
+    # The noise "estimate" is simply the average of the noise power
+    # spectral density in the frame:
+    P_N = Y_N2.mean(axis=-1)
+    Y_hat = Y2 - factor * P_N[:, np.newaxis]     # subtraction
+    PN_hat = Y_N2 - factor * P_N[:, np.newaxis]  # subtraction for noise alone
+    Y_hat = np.maximum(Y_hat, 0)  # Make the minima equal zero
+    PN_hat = np.maximum(PN_hat, 0)
+    Y_hat[0:2, :] = 0
+    PN_hat[0:2, :] = 0
+
+    # Combining the estimated power spectrum with the original noisy phase,
+    # and add the frames using an overlap-add technique
+    output_Y = overlap_and_add(np.sqrt(Y_hat), YPhase,
+                               (w + padz), shift_p * w)
+    output_N = overlap_and_add(np.sqrt(PN_hat), Y_NPhase,
+                               (w + padz), shift_p * w)
+    return output_Y, output_N
 
 
 def overlap_and_add(powers, phases, len_window, shift_size):
--- test_distort.py
+++ test_distort.py
@@ -19,3 +19,15 @@ def test_overlap_and_add(mat_overlap_and_add):
     y = distort.overlap_and_add(powers, phases, len_window,  shift)
     np.testing.assert_allclose(y, target, atol=1e-15)
 
+
+def test_spec_sub(mat_spec_sub):
+    signal = mat_spec_sub['input']
+    noise = mat_spec_sub['noise']
+    w = mat_spec_sub['W'].squeeze()
+    padz = mat_spec_sub['padz'].squeeze()
+    shift = mat_spec_sub['SP'].squeeze()
+    factor = mat_spec_sub['factor'].squeeze()
+    target = mat_spec_sub['output_Y'].squeeze()
+    target_noise = mat_spec_sub['output_N'].squeeze()
+    y_speech, y_noise = distort.spec_sub(signal, noise, factor, w, padz, shift)
+    np.testing.assert_allclose(y_speech, target)
