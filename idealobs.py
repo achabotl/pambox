be4c1a622c4b69047824d37236c57d43e607be0c
Alexandre Chabot-Leclerc <alexandre.chabot.leclerc@gmail.com>
2013-10-07 16:18:58 +0200

    Converted the IdealObs to a class

--- idealobs.py
+++ idealobs.py
@@ -8,17 +8,11 @@ from numpy import array
 from numpy import linspace
 
 
-# Defaults
-K = sqrt(1.2)
-Q = 0.5
-SIGMA_S = 0.6
-M = 8000.0
-
 class IdealObs(object):
 
     """Docstring for IdealObs. """
 
-    def __init__(self, k=sqrt(1.2), sigma_s=0.5, q=0.6, m=8000.):
+    def __init__(self, k=sqrt(1.2), q=0.5, sigma_s=0.6, m=8000.):
         """@todo: to be defined1.
 
         :k: @todo
@@ -28,9 +22,12 @@ class IdealObs(object):
 
         """
         self.k = k
-        self.sigma_s = sigma_s
         self.q = q
+        self.sigma_s = sigma_s
         self.m = m
+
+    def get_params(self):
+        return {'k': self.k, 'q': self.q, 'sigma_s': self.sigma_s, 'm': self.m}
 
     def fit_obs(self, snrenv, pcdata):
         """Find the optimal parameters for the ideal observer.
@@ -38,22 +35,53 @@ class IdealObs(object):
             correct.
         pcdata: the data, in percentage of correctly understood tokens.
         """
-        errfc = lambda p, snr, data: snrenv_to_pc(snrenv, p[0], p[1], p[2], M) - data
-        p0 = [K, Q, SIGMA_S]
-        res = leastsq(errfc, p0, args=(snrenv, pcdata))
-        return res
+        errfc = lambda p, snr, data: self._snrenv_to_pc(snrenv, p[0], p[1],
+                                                        p[2], self.m) - data
+        p0 = [self.k, self.q, self.sigma_s]
+        res = leastsq(errfc, p0, args=(snrenv, pcdata))[0]
+        self.k, self.q, self.sigma_s = res
+        return self
 
-def snrenv_to_pc(snrenv, k=K, q=Q, sigma_s=SIGMA_S, m=M):
-    """Convert SNR_env values to a percent correct using a ideal observer
-    snrenv: linear values of SNRenv
-    Reference: Jorgensen and Dau (2011)
-    Author: Alexandre Chabot-Leclerc (alech AT elektro.dtu.dk)
-    """
-    Un = norm.ppf(1.0 - 1.0 / m)
-    sn = 1.28255 / Un
-    un = Un + 0.577 / Un
-    dp = k * snrenv**q
-    return norm.cdf(dp, un, sqrt(sigma_s**2 + sn**2)) * 100
+    def _snrenv_to_pc(self, snrenv, k, q, sigma_s, m):
+        """Convert SNR_env values to a percent correct using a ideal observer
+
+        Uses specified parameters of the ideal observer so that we can do the
+        parameter optimization.
+
+        Parameters
+        ----------
+        snrenv : array_like
+            linear values of SNRenv
+
+        Returns
+        -------
+        pc : array_like
+            Percentage of correctly understood speech units.
+
+        """
+        Un = norm.ppf(1.0 - 1.0 / m)
+        sn = 1.28255 / Un
+        un = Un + 0.577 / Un
+        dp = k * snrenv ** q
+        return norm.cdf(dp, un, sqrt(sigma_s ** 2 + sn ** 2)) * 100
+
+    def snrenv_to_pc(self, snrenv):
+        """Convert SNR_env values to a percent correct using a ideal observer
+
+
+        Parameters
+        ---------
+        snrenv : array_like
+            linear values of SNRenv
+
+        Returns
+        -------
+        pc : ndarray
+            Percentage of correctly understood speech units.
+
+        Reference: Jorgensen and Dau (2011)
+        """
+        return self._snrenv_to_pc(snrenv, self.k, self.q, self.sigma_s, self.m)
 
 
 def psy_fn(x, mu=0, sigma=1):
@@ -74,17 +102,20 @@ if __name__ == '__main__':
     from pylab import plot, figure, show, xlabel, ylabel, legend
     snr = arange(-9, 3, 1)
     snrenv = 10 ** linspace(-2, 2, len(snr))
+    c = IdealObs()
     data = psy_fn(snr, -3.1, 2.13) * 100
-    popt = fit_obs(snrenv, data)
+    c.fit_obs(snrenv, data)
 
     figure
     plot(snr, data, 'b--', label='Data')
-    plot(snr, snrenv_to_pc(snrenv, *popt[0], m=M), 'r--', label='Model')
+    plot(snr, c.snrenv_to_pc(snrenv), 'r--', label='Model')
     xlabel('SNR [dB]')
     ylabel('Percent correct')
     legend(loc='upper left')
     show()
-    k, q, ss = popt[0]
-    print(('Optimized parameters found:\nk = {:.3f}\nq = {:.3f}\n'
-           'sigma_s = {:.3f}\nm = {:.3f}').format(k, q, ss, M))
 
+    params = c.get_params()
+    print(('Optimized parameters found:\nk = {:.3f}\nq = {:.3f}\n'
+           'sigma_s = {:.3f}\nm = {:.3f}').format(params['k'], params['q'],
+                                                  params['sigma_s'],
+                                                  params['m']))
