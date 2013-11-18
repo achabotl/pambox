15aa9417c1b9102facdb59bbeb00710690fba1dd
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2013-11-18 15:47:05 +0100

    Direct import of assert_all close. Squeeze directly on matlab import

--- tests/test_auditory.py
+++ tests/test_auditory.py
@@ -2,13 +2,15 @@ import pytest
 import numpy as np
 from pambox import auditory as aud
 import scipy.io as sio
+from numpy.testing import assert_allclose
 
 
 def test_lowpass_filtering_of_envelope():
-    mat = sio.loadmat("./test_files/test_envelope_filtering.mat")
-    envelope = mat['envelope']
-    target = mat['filtered_envelope']
+    mat = sio.loadmat("./test_files/test_hilbert_env_and_lp_filtering_v1.mat",
+                      squeeze_me=True)
+    envelope = mat['unfiltered_env']
+    target = mat['lp_filtered_env']
     filtered_envelope = aud.lowpass_env_filtering(envelope, 150., 1, 22050.)
-    np.testing.assert_allclose(filtered_envelope, target, atol=1e-7)
+    assert_allclose(filtered_envelope, target, atol=1e-7)
 
 
