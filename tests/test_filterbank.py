10c9a04a16792dc922073032080d8d8491854e79
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2013-12-13 10:31:06 +0100

    Use numpy fft in filterbank and asarray instead of array

--- tests/test_filterbank.py
+++ tests/test_filterbank.py
@@ -16,11 +16,11 @@ def test_third_octave_filtering_of_noise_():
 
 
 def test_mod_filtering_for_simple_signal():
-    signal = np.array([1, 0, 1, 0, 1])
+    signal = np.asarray([1, 0, 1, 0, 1])
     fs = 2205
-    modf = np.array([1., 2., 4., 8., 16., 32., 64.])
+    modf = np.asarray([1., 2., 4., 8., 16., 32., 64.])
     p = filterbank.mod_filterbank(signal, fs, modf)
-    target = np.array([6.69785298e-18, 6.06375859e-06, 2.42555385e-05,
+    target = np.asarray([6.69785298e-18, 6.06375859e-06, 2.42555385e-05,
                       9.70302212e-05, 3.88249957e-04, 1.55506496e-03,
                       6.25329663e-03])
     assert_allclose(p, target, rtol=1e-2)
