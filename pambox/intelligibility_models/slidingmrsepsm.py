684cae3e0309d5ef08421e6e177118e6017f328f
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2014-05-01 13:45:39 +0200

    Working mr-sEPSM with sliding mr-window.
    Implementation of the mr-sEPSM with a sliding window for the computation
    of the multi-resolution envelope power calculation. The step size is set
    by the smallest window, associated with the highest modulation frequency
    (e.g. 1/256 ms). The window is centered at the index of the step. This
    means that the first windows are incomplete for each modulation center
    frequency, because half of the window falls "out" of the array because
    it is "before" the start of the signal. This is not so noticeble for
    high modulation frequencies, but it is easier to observer for the lowest
    modulation frequency, which could have only 1 or 2 full windows for a
    given sentence.
    
    The current implementation is rather slow. The sliding window is
    implemented as a generator. I can kind of thing of a way of doing it
    with numpy's strides or reshape, but I won't implement it now.

---
+++ pambox/intelligibility_models/slidingmrsepsm.py
@@ -0,0 +1,56 @@
+from __future__ import division, print_function, absolute_import
+from itertools import izip
+import numpy as np
+from pambox.intelligibility_models.mrsepsm import MrSepsm
+
+
+class SlidingMrSepsm(MrSepsm):
+    _default_modf = (1., 2., 4., 8., 16., 32., 64., 128., 256.)
+
+
+    def __init__(self, fs=22050,
+                 cf=(63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000,
+                1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000),
+                 modf=(1., 2., 4., 8., 16., 32., 64., 128., 256.),
+                 downsamp_factor=10,
+                 noise_floor=0.001,
+                 snr_env_limit=0.001):
+        self.mr = super(SlidingMrSepsm,
+                        self).__init__(fs=fs,
+                                       cf=cf,
+                                       modf=modf,
+                                       downsamp_factor=downsamp_factor,
+                                       noise_floor=noise_floor,
+                                       snr_env_limit=snr_env_limit)
+
+    @staticmethod
+    def _inc_sliding_window(x, win=2, step=1):
+        n = len(x)
+        step = int(step)
+        win = int(win)
+        for i in range(0, n, step):
+            # Use python's max to avoid conversion to array.
+            i_beg = max([0, i - win // 2])
+            i_end = min([i + win // 2, n])
+            yield x[i_beg:i_end]
+
+    def _mr_env_powers(self, channel_env, filtered_envs):
+        len_env = filtered_envs.shape[-1]
+        win_durations = 1. / np.asarray(self.modf, dtype='float')
+        win_lengths = np.floor(win_durations * self.fs / self.downsamp_factor)
+        swin_length = win_lengths.min()
+        n_segment = np.ceil(len_env / swin_length)
+
+        # DC power used for normalization. Divided by 2 such that a fully
+        # modulated signal has # an AC-power of 1.
+        dc_power = np.mean(channel_env) ** 2 / 2
+
+        # Create a masked array of zeros, where all entries are hidden.
+        mr_env_powers = np.zeros((len(self.modf), n_segment))
+
+        for i_modf, (env, win_length) in enumerate(
+                izip(filtered_envs, win_lengths)):
+            win_iter = self._inc_sliding_window(env, win_length, swin_length)
+            for i_win, each in enumerate(win_iter):
+                mr_env_powers[i_modf, i_win] = np.var(each, ddof=1) / dc_power
+        return mr_env_powers
