afecc953a6449e1c9e2e5e5b178e957514d05a73
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2014-05-08 11:30:52 +0200

    Define __all__ in intelligibility_models package
    Define to import all intelligibility models available if one imports
    with *. Also, allows to import the models directly from
    intelligibility_models, rather than having to import the modules.

--- pambox/intelligibility_models/__init__.py
+++ pambox/intelligibility_models/__init__.py
@@ -1,2 +1,13 @@
-import sepsm
-import sii
+"""
+The :mod:`pambox.intelligibility_modesl` module gather speech intelligibility
+models.
+"""
+from .mrsepsm import MrSepsm
+from .sepsm import Sepsm
+from .sii import Sii
+
+__all__ = ['Sepsm',
+           'MrSepsm',
+           'Sii']
+
+
