f90a100f820cac097b1f8751edc62841fbb259cc
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2014-05-13 13:59:57 +0200

    Add numpydoc style documentation to Ideal Obs
    Also replace explicit imports of some numpy function to simply use `import numpy as np`.
    #
    # Why is this change necessary?
    # How does it address the issue?
    # What side effects does this change have?
    # Consider including a link \[to the discussion.\]
    #
    # Last 5 commit messages
    # ----------------------
    # 9ca76ab Merge branch 'sliding-mr-sepsm' into develop [Alexandre Chabot-Leclerc]
    # d86f0ac Define Hilbert transform done on last dimension. [Alexandre Chabot-Leclerc]
    # 6221cdf Define __all__ in intelligibility_models package [Alexandre Chabot-Leclerc]
    # 2360fab Use correct values in setup file to upload to PyPi [Alexandre Chabot-Leclerc]
    # 305217f Remove mr optimal combination. [Alexandre Chabot-Leclerc]

--- pambox/idealobs.py
+++ pambox/idealobs.py
@@ -1,39 +1,100 @@
 # -*- coding: utf-8 -*-
-from __future__ import division
+"""
+Classes and functions related to the concepts of ideal observer and
+psychometric functions.
+"""
+from __future__ import division, print_function
+
+import numpy as np
 from scipy.stats import norm
 from scipy.optimize import leastsq
 from scipy.special import erfc
-from numpy import sqrt
-from numpy import arange
-from numpy import linspace
 
 
 class IdealObs(object):
+    """
+    Statistical ideal observer.
 
-    """Docstring for IdealObs. """
+    Converts input values (usually SNRenv) to a percentage.
 
-    def __init__(self, k=sqrt(1.2), q=0.5, sigma_s=0.6, m=8000.):
-        """@todo: to be defined1.
+    Parameters
+    ----------
+    k : float, optional
+         (Default value = sqrt(1.2)
+    q : float, optional
+        (Default value = 0.5)
+    sigma_s : float, optional
+        (Default value = 0.6)
+    m : int, optional
+        Number of words in the vocabulary. (Default value = 8000)
 
-        :k: @todo
-        :sigma_s: @todo
-        :q: @todo
-        :m: @todo
+    Examples
+    --------
 
-        """
+    Converting SNRenv values to percent correct using the default parameters
+    of the ideal observer:
+
+    >>> from pambox import idealobs
+    >>> obs = idealobs.IdealObs()
+    >>> obs.snrenv_to_pc((0, 1, 2, 3))
+
+    References
+    ----------
+
+    .. [JD11] Jørgensen, Søren, and Torsten Dau. "Predicting speech
+        intelligibility based on the signal-to-noise envelope power ratio
+        after modulation-frequency selective processing." The Journal of the
+        Acoustical Society of America 130.3 (2011): 1475-1487.
+
+    """
+    def __init__(self, k=np.sqrt(1.2), q=0.5, sigma_s=0.6, m=8000.):
         self.k = k
         self.q = q
         self.sigma_s = sigma_s
         self.m = m
 
     def get_params(self):
+        """
+        Returns the parameters of the ideal observer as dict.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        params : dict
+            Dictionary of internal parameters of the ideal observer.
+        """
         return {'k': self.k, 'q': self.q, 'sigma_s': self.sigma_s, 'm': self.m}
 
     def fit_obs(self, snrenv, pcdata, sigma_s=None, m=None):
-        """Find the optimal parameters for the ideal observer.
-        snrenv: the linear SNRenv values that are to be converted to percent
+        """
+        Finds the paramaters `k`, `q`, and `sigma_s`, that minimize the
+        least-square error between a data set and transformed SNRenv.
+
+        By default the `m` parameter is fixed and the property `m` is used.
+        It can also be defined as an optional parameter.
+
+        It is also possible to fix the `sigma_s` parameter by passing it as
+        an optional argument. Otherwise, it is optimized with `k` and `q`.
+
+
+        Parameters
+        ----------
+        snrenv : ndarray
+            The linear SNRenv values that are to be converted to percent
             correct.
-        pcdata: the data, in percentage of correctly understood tokens.
+        pcdata : ndarray
+            The data, in percentage between 0 and 1, of correctly understood
+            tokens. Must be the same shape as `snrenv`.
+        sigma_s : float, optional
+             (Default value = None)
+        m : float, optional
+             (Default value = None)
+
+        Returns
+        -------
+
         """
 
         if not m:
@@ -67,73 +128,85 @@ class IdealObs(object):
 
     @staticmethod
     def _snrenv_to_pc(snrenv, k=None, q=None, sigma_s=None, m=None):
-        """Convert SNR_env values to a percent correct using a ideal observer
-
-        Uses specified parameters of the ideal observer so that we can do the
-        parameter optimization.
+        """
+        Converts SNRenv values to percent correct using an ideal observer.
 
         Parameters
         ----------
         snrenv : array_like
             linear values of SNRenv
         k : float
-            k parameter
+            k parameter (Default value = None)
         q : float
-            q parameter
+            q parameter (Default value = None)
         sigma_s : float
-            sigma_s parameter
+            sigma_s parameter (Default value = None)
         m : float
-            m parameter, number of words in the vocabulary.
+            m parameter, number of words in the vocabulary. (Default value = None)
 
         Returns
         -------
-        pc : array_like
-            Percentage of correctly understood speech units.
+        pc : ndarray
+            Array of intelligibility percentage values, of the same shape as
+            `snrenv`.
 
         """
         Un = norm.ppf(1.0 - 1.0 / m)
         sn = 1.28255 / Un
         un = Un + 0.577 / Un
         dp = k * snrenv ** q
-        return norm.cdf(dp, un, sqrt(sigma_s ** 2 + sn ** 2)) * 100
+        return norm.cdf(dp, un, np.sqrt(sigma_s ** 2 + sn ** 2)) * 100
 
     def snrenv_to_pc(self, snrenv):
-        """Convert SNR_env values to a percent correct using a ideal observer
-
+        """
+        Converts SNRenv values to a percent correct using the parameters of
+        the ideal observer.
 
         Parameters
-        ---------
+        ----------
         snrenv : array_like
             linear values of SNRenv
 
         Returns
         -------
         pc : ndarray
-            Percentage of correctly understood speech units.
+            Array of intelligibility percentage values, of the same shape as
+            `snrenv`.
 
-        Reference: Jorgensen and Dau (2011)
+        
         """
+        snrenv = np.asarray(snrenv)
         return self._snrenv_to_pc(snrenv, self.k, self.q, self.sigma_s, self.m)
 
 
-def psy_fn(x, mu=0, sigma=1):
-    """Psychometric function
-    Convert x values to percent correct
-    Inputs:
-    x: the input array
-    mu: mean of the distribution (default 0)
-    sigma: standard deviation of the distribution (default 1)
-
-    Outputs:
-    pc: array of values between 0 and 100
+def psy_fn(x, mu=0., sigma=1.):
     """
-    return 100 * erfc(-(x - mu) / (sqrt(2) * sigma)) / 2
+    Calculates a psychometric function with a given mean and variance.
+
+    Parameters
+    ----------
+    x : array_like
+        "x" values of the psychometric functions.
+    mu : float, optional
+        Value at which the psychometric function reaches 50%, i.e. the mean
+        of the distribution. (Default value = 0)
+    sigma : float, optional
+        Variance of the psychometric function. (Default value = 1)
+
+    Returns
+    -------
+    pc : ndarray
+        Array of "percent correct", between 0 and 100.
+
+    """
+    x = np.asarray(x)
+    return 100 * erfc(-(x - mu) / (np.sqrt(2) * sigma)) / 2
 
 
 if __name__ == '__main__':
     from pylab import plot, figure, show, xlabel, ylabel, legend, xticks, yticks
-    snr = arange(-12, 5, 1)
-    snrenv = 10 ** linspace(-2, 2, len(snr))
+    snr = np.arange(-12, 5, 1)
+    snrenv = 10 ** np.linspace(-2, 2, len(snr))
     c = IdealObs()
     data = psy_fn(snr, -3.1, 2.13) * 100
     c.fit_obs(snrenv, data)
