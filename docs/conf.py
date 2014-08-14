5e18ff2685c293e2d773b24935f678ab867ca0af
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2014-08-14 19:01:02 +0200

    Remove mocks in sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,6 @@ import sys, os
 #sys.path.insert(0, os.path.abspath('.'))
 
 
-# Mock import so that the documentation can be build on ReadTheDocs.org
-import mock
-
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'pandas']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
