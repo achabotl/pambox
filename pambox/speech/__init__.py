918c6adffa8edecda75115f6480f1e1d255abc56
Alexandre Chabot-Leclerc <alex@alexchabot.net>
2014-10-07 17:43:20 +0200

    Add Binaural mr-sEPSM to __init__

--- pambox/speech/__init__.py
+++ pambox/speech/__init__.py
@@ -4,6 +4,7 @@ models.
 """
 from __future__ import absolute_import
 
+from .binauralmrsepsm import BinauralMrSepsm
 from .sepsm import Sepsm
 from .mrsepsm import MrSepsm
 from .sii import Sii
@@ -11,6 +12,7 @@ from .material import Material
 from .experiment import Experiment
 
 __all__ = [
+    'BinauralMrSepsm',
     'Sepsm',
     'MrSepsm',
     'Sii',
