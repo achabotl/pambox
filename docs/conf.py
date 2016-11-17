56c8978a2617ec787605df85d6773d8aebd75a1f
Alexandre Chabot-Leclerc <achabot@enthought.com>
2016-11-16 21:42:30 -0800

    DOC: Change default theme to Alabaster

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     html_theme = 'default'
 else:
-    html_theme = 'nature'
+    html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
